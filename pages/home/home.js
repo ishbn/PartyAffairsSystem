3913cf4c99ef9110e824bfe1e31f1ddb8df97b4e
hbn <hyqhbn@gmail.com>
2018-07-13 00:14:33 +0800

    Initial commit

---
+++ pages/home/home.js
@@ -0,0 +1,121 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    broadcast: [
+      '/images/background/dj.png',
+      '/images/background/dj1.png',
+      '/images/background/dj.png',
+      '/images/background/dj1.png'
+    ],
+    menu: {
+      imgUrls: [
+        '/images/icon_function/file.png',
+        '/images/icon_function/partyClass.png',
+        '/images/icon_function/threeOne.png',
+        '/images/icon_function/taskManagement.png',
+        '/images/icon_function/notice.png',
+        '/images/icon_function/partyNews.png',
+        '/images/icon_function/examination.png',
+        '/images/icon_function/thoughtReport.png',
+        '/images/icon_function/taskManagement.png',
+        '/images/icon_function/more.png'
+      ],
+      descs: [
+        '党员档案',
+        '微党课',
+        '三会一课',
+        '专题教育',
+        '我的通知',
+        '党建要闻',
+        '在线考试',
+        '思想汇报',
+        '任务管理',
+        '分类'
+      ]
+    },
+    list_news:[
+      {
+        title:"在习近平党建思想指引下实干担当",
+        date:'2018-07-11',
+        image:'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
+      },
+      {
+        title: "如何增强抓落实的本领？",
+        date: '2018-07-11',
+        image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
+      }
+    ],
+  list_notices: [
+    {
+      title: "在习近平党建思想指引下实干担当",
+      date: '2018-07-11',
+      image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
+    },
+    {
+      title: "如何增强抓落实的本领？",
+      date: '2018-07-11',
+      image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
+    }
+  ]
+},
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ pages/news/news_home/news_home.js
@@ -0,0 +1,100 @@
+// pages/news/partySchool_home/partySchool_home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imges:'/images/background/news/img_header.png',
+    menu: {
+      imgUrls: [
+        '/images/icon_function/partyNews.png',
+        '/images/icon_function/notice.png',
+        '/images/icon_function/publicity.png',
+        '/images/icon_function/hero.png'
+      ],
+      descs: [
+        '党内要闻',
+        '通知公告',
+        '党内公示',
+        '党史人物'
+      ],
+      explain:[
+        '新闻直播间，了解党内事',
+        '最新通知，一键查看',
+        '党内动态，即时了解',
+        '缅怀先辈，勿忘感恩'
+      ],
+      targetPages:[
+        "./../news/news_list/news_list",
+        "./../notices/notices_list/notices_list",
+        "./../notices/notice_detail/notice_detail",
+        "./../notices/notice_detail/notice_detail"
+      ]
+    },
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  menuTargetTo:function(e){
+    console.log(e);
+    var src=e.target.dataset.targeturl;
+    console.log(src);
+    wx.navigateTo({
+      url: src
+    })
+  }
+})
