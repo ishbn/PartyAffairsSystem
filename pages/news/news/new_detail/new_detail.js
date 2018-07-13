40cfa9f2d004a12a7dfbdb57794d3aa61cb816ed
hbn <hyqhbn@gmail.com>
2018-07-13 14:58:52 +0800

    小程序文章内容富文本解析
    本次提交主要添加了wxParse进行富文本文章内容解析，具体使用方法参考https://github.com/icindy/wxParse

---
+++ pages/news/news/new_detail/new_detail.js
@@ -0,0 +1,79 @@
+// pages/news/news/new_detail/new_detail.js
+var WxParse = require('../../../../utils/wxParse/wxParse.js');
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    article:{
+      news_id: '7',
+      title: "如何增强抓落实的本领？",
+      content: "<p><span style='font-size: 14px; text-align: justify;'>人生最大的快乐不在于占有什么，而在于追求什么的过程。</span></p>",
+      date: '2018-07-11',
+      image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg',
+      source: '',
+      comment: 0,
+      praise: 10,
+    },
+    icon_comt:'/images/icon_base/icon-comt.png',
+    icon_priaze:'/images/icon_base/icon-priaze.png'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    WxParse.wxParse('article.content', 'html', that.data.article.content, that, 5);
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
--- pages/news/news/news_list/news_list.js
+++ pages/news/news/news_list/news_list.js
@@ -12,41 +12,49 @@ Page({
     ],
     list_news: [
       {
+        news_id:'1',
         title: "在习近平党建思想指引下实干担当",
         date: '2018-07-11',
         image: '/images/background/dj1.png'
       },
       {
+        news_id: '2',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
       },
       {
+        news_id: '3',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
       },
       {
+        news_id: '4',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
       },
       {
+        news_id: '5',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
       },
       {
+        news_id: '6',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
       },
       {
+        news_id: '7',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
       },
       {
+        news_id: '8',
         title: "如何增强抓落实的本领？",
         date: '2018-07-11',
         image: 'http://img02.tooopen.com/images/20141231/sy_78327074576.jpg'
@@ -108,5 +116,18 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  showDetail:function(){
+    wx.navigateTo({
+      url: '../new_detail/new_detail',
+      success: function(res) {},
+      fail: function(res) {
+        wx.showToast({
+          title: '暂时无法打开',
+          icon:'none'
+        })
+      },
+      complete: function(res) {},
+    })
   }
 })
